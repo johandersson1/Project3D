8f39433ec50cbe9d1822c56db5f7005064b35c83
johandersson1 <johan_bst@hotmail.com>
2021-07-09 15:39:45 +0200

    shadowmap

--- Lights.h
+++ Lights.h
@@ -20,4 +20,9 @@ struct DirectionalLight
 	float pad;
 	float pad2;
 	float pad3;
+
+	DirectionalLight()
+		:ambient({ 0.5f,0.5f,0.5f,1.0f }), diffuse({ 0.7f,0.7f,0.7f,1.0f }), specular({ 1.0f,1.0f,1.0f,1.0f, }), position({ 0.0f,20,20, 1 }),
+		camPosition({ 0,0,0,0 }), lightDirection({ 0.0f,-0.5f, -1.0f, 1.0f }), range(10), strength(2.5f), att0(0), att1(1), att2(0) {};
+
 };
