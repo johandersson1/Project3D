7cc5e67e6b7e720ec40c05615b0d6577d42546d8
johandersson1 <johan_bst@hotmail.com>
2021-07-09 15:40:35 +0200

    shadow

--- ShadowMapVertex.hlsl
+++ ShadowMapVertex.hlsl
@@ -1,4 +1,24 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+cbuffer constantBuffer : register(b0)
 {
-	return pos;
+    float4x4 WVP; 
+    float4x4 worldSpace;
+}
+struct VertexShaderInput
+{
+    float3 position : POSITION;
+ 
+};
+struct VertexShaderOutput
+{
+    float4 position : SV_POSITION;
+    float2 uv : UV;
+    float3 normal : NORMAL;
+};
+
+VertexShaderOutput main(VertexShaderInput input)
+{
+    VertexShaderOutput output;
+    output.position = mul(float4(input.position, 1), mul(worldSpace, WVP));
+    
+    return output;
 }
